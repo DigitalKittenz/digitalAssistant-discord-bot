5420c6d3e9f7537fda53eba9c10b6173475b30c2
DigitalKittenz <114578234+DigitalKittenz@users.noreply.github.com>
2023-07-27 01:34:53 +0100

    added stuff to index

--- index.js
+++ index.js
@@ -7,6 +7,18 @@ const client = new Client({ intents: [GatewayIntentBits.Guilds]});
 
 client.commands = new Collection();
 
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+for (const fie of commandFiles){
+	const filePath = path.join(commandsPath,file);
+	const command = require(filepath);
+	if ('data' in command && 'execute' in command){
+		client.commands.set(command.data.name, command);
+	} else{
+		console.log('[WARNING] The command at ${filePath} is missing a required "data" or "execute" propery');
+	}
+}
 
 
 
@@ -24,3 +36,10 @@ client.once(Events.ClientReady, c => {
 
 // Log in to Discord with your client's token
 client.login(token);
+
+
+
+client.on(Events.InteractionCreate, interaction => {
+	if (!interaction.isChatInputCommand()) return;
+	console.log(interaction);
+});
