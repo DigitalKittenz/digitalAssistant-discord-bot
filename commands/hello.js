1d986faa5b8935c2c559006ae460f834f0670c15
DigitalKittenz <114578234+DigitalKittenz@users.noreply.github.com>
2023-07-27 23:54:21 +0100

    put the commands in a separate folder

---
+++ commands/hello.js
@@ -0,0 +1,19 @@
+// hello.js
+module.exports = {
+    name: 'hello',
+    description: 'Says hello!',
+    execute(interaction) {
+        // randomize the response
+        const options = [
+            'hiii <3',
+            'hii. i‛m not a bot, btw. swear 🤞',
+            'hey whats up? r u gonna do something or just say hi to me lol',
+            'i‛m bored 😔',
+            '😊👋',
+            'hello!',
+            'da bot? more like, daring and awesome bot, am i right? right guys? guys? 😢 where is everyone?',
+            'heya! i‛m totally not a bot, btw, swear! i have crippling self-doubt just like you!',
+        ];
+        interaction.reply(options[Math.floor(Math.random() * options.length)]);
+    }
+};
--- index.js
+++ index.js
@@ -1,49 +1,48 @@
+// index.js
 // reload discord client
 const { Client, GatewayIntentBits } = require('discord.js');
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const { Collection } = require('discord.js');
+
 // intents are needed to register a client, apparently?
+
+// require fs module
+const fs = require('fs');
+
+// create a collection to store commands
+client.commands = new Collection();
+
+// loop through the files in the commands folder and add them to the collection
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+for (const file of commandFiles) {
+    const command = require(`./commands/${file}`);
+    client.commands.set(command.name, command);
+}
 
 // listen and see when connected
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
 });
 
-// okay commands now. this is an array of stuff
-const options = [
-    'hiii <3',
-    'hii. i‛m not a bot, btw. swear 🤞',
-    'hey whats up? r u gonna do something or just say hi to me lol',
-    'i‛m bored 😔',
-    '😊👋',
-    'hello!',
-    'da bot? more like, daring and awesome bot, am i right? right guys? guys? 😢 where is everyone?',
-    'heya! i‛m totally not a bot, btw, swear! i have crippling self-doubt just like you!',
-];
-
 // listen to interactions and fire an event 🔥
 client.on('interactionCreate', async (interaction) => {
     if (!interaction.isCommand()) return;
 
-    if (interaction.commandName === 'hello') {
-        // randomize the response
-        await interaction.reply(
-            options[Math.floor(Math.random() * options.length)]
-        );
-    } else if (interaction.commandName === 'manul') {
-        // define a list of manul pics
-        const pics = [
-            'https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExenJkMGw5Ymd3bnI1cW1qd2NvZ3k2bGZiamw0bmpsbTR4M2piZWIzdiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/F0ZriKBdzZjnARVTeM/giphy.gif',
-            'https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExNmhzeW03dmV6czM0Ymp5bmRjcjM2aWl4bnNxbzlsMzZkaWRoc3I1byZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/L4G60Ewtdoem4/giphy.gif',
-            'https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExZGZ4M3dwc2NoMXdxd2ltOGR3MHlxcDYyd2Y2Y3JsZGVqdDg1bzkxeCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/YL8quuOin5E7C/giphy.gif',
-            'https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExNTVoZW85b3lwajhtY2tpYjA4N20xeWJ0eWh2OHAyeGt2N3NobXlvZyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/K8l4eiS88UvWU/giphy.gif',
-            'https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExN2U5c2piNG1odWJ2cTBycm9xcXA5Y2ZkOXN0eDNrcHN3NXY0Z2lnaSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/12fjBkQf5CV596/giphy.gif'
-        ];
+    // get the command name from the interaction
+    const commandName = interaction.commandName;
 
-        // choose a random manul pic
-        const pic = pics[Math.floor(Math.random() * pics.length)];
+    // check if the command exists in the collection
+    if (!client.commands.has(commandName)) return;
 
-        // send a response with the manul pic
-        await interaction.reply(`here's a manul pic for u <3: ${pic}`);
+    // get the command from the collection
+    const command = client.commands.get(commandName);
+
+    // try to execute the command
+    try {
+        await command.execute(interaction);
+    } catch (error) {
+        console.error(error);
+        await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
     }
 });
 
