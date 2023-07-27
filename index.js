b029a4e921f2d93574e51b698a55ef9f2e94b51f
DigitalKittenz <114578234+DigitalKittenz@users.noreply.github.com>
2023-07-27 16:02:50 +0100

    added a blank array
    just thinking of stuff to put in here

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
 // reload discord client
 const { CLient, Intents } = require('discord.js');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+// intents is needed to register a client apparently?
+
+// listen and see when connected
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+});
+
+// okay commands now. this is an array of stuff
+
+const options = [
+    
+
+
+
+
+]
