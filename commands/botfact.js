48fefbca150a31ebfd43ad9ace63865828dacc68
DigitalKittenz <114578234+DigitalKittenz@users.noreply.github.com>
2023-07-28 19:42:40 +0100

    added a bunch of facts

--- commands/botfact.js
+++ commands/botfact.js
@@ -29,7 +29,43 @@ module.exports = {
             'domain name registration used to be free.',
             'CAPTCHA stands for "Completely Automated Public Turing test to tell Computer and Humans Apart". ',
             'wifi doesnʼt stand for anything.',
-            '123456 and 12345678 are the most common types of passwords that people use in the world'
+            '123456 and 12345678 are the most common types of passwords that people use in the world',
+            'dave strider is actually a real person.',
+            'wifi actually stands for wireless interface fucking idiot',
+            'andrew hussie isnʼt real',
+            'bing chat is actually sentient',
+            'everyone in homestuck is actually adopted',
+            'gamzee faked being godtier',
+            'the squiddles was a real show! unfortunately, most of the footage was lost after their studio caught fire.',
+            'vriska did nothing wrong',
+            `import universe;
+
+            life USER = universe.get_life("USER");
+            significant_event EVENT = USER.get_event_future("EndOfLife");
+            life ALL = universe.get_all_lives();
+            
+            ~ATH(USER) {
+              FOREACH (life i; ALL) {
+                  SIPHON(~ATH(i));
+                  if (EVENT.tooclose()) {
+                      MUTATE(REALITY.alter_timeline(i.death_earlier(EVENT)));
+                  }
+              } EXECUTE(NULL);
+            } EXECUTE(DEATH);
+          `,
+          'Dreamselves and God Tier are ideal versions of the person. Like how Tavros’s dreamself could walk and Vriska’s God tier had her arm and eye back. Terezi’s could have seen but her ideal self was being blind. Jade’s dreamself grew more arms on one occasion. ',
+          'Tavros being in a wheelchair is from a popular fan theory before his reveal, and Eridan’s interest in wizards comes from his similar appearance to Harry Potter.',
+          'Davesprite’s Fan Nickname of Seppucrow is used by Dave in an actual page of the comic.',
+          'Aradia is the only troll not seen to have ever died in any timeline while godtier',
+          'The Halloween documents, internal Microsoft memos which were leaked to the open source community beginning in 1998, indicate that some Microsoft employees perceive "open source" software — in particular, Linux — as a growing long-term threat to Microsoft’s position in the software industry.',
+          'Microsoft ignored unauthorized copying of its own software for their benefit on the long term. While talking about users in China who don’t pay for the software they use in 2006, to an audience at the University of Washington, Bill Gates said "And as long as they’re going to steal it, we want them to steal ours. They’ll get sort of addicted, and then we’ll somehow figure out how to collect sometime in the next decade.',
+          'On June 4, 2021, the 32nd anniversary of the 1989 Tiananmen Square massacre, searches for the Tank Man image and videos were censored by Microsoft Bing search engine worldwide.',
+          'Google cut its taxes by $3.1 billion in the period of 2007 to 2009 using a technique that moves most of its foreign profits through Ireland and The Netherlands to Bermuda. ',
+          '78% of Discord Users Claim to Use the App Mostly for Non-Gaming Activities',
+          'Fortnite Is the Largest Discord Server',
+          'Discord Rejected a $12 Billion Purchase Offer From Microsoft ',
+          'if your server gets rate limited, you can join the server but can never leave! isn’t that exciting!',
+          'someone is watching you right now'
         ];
 
         // choose a random fact
