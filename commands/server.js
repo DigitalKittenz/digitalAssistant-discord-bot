df670615210fa6c413c5175c4aa354b08af73004
DigitalKittenz <114578234+DigitalKittenz@users.noreply.github.com>
2023-07-29 23:19:51 +0100

    it got so broken hope this works

--- commands/server.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-		name: 'server',
-		description: 'displays server info',
-		execute(interaction){
-
-		interaction.reply(`Server name: ${interaction.guild.name}\nTotal members: ${interaction.guild.memberCount}`);
-	},
-};
