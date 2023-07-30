7a5cfb775f5edd5a3e4dd767a30ea15db1c27da9
DigitalKittenz <114578234+DigitalKittenz@users.noreply.github.com>
2023-07-30 03:22:29 +0100

    removed the lru thing and using maps instead

--- events/message.js
+++ events/message.js
@@ -1,53 +1,46 @@
-const url = require('url');
-const QuickLRU = require('quick-lru');
-
 module.exports = {
     name: 'messageCreate',
     messageMap: new Map(),
     urlMap: new Map(),
     channels: process.env.CHANNEL_IDS.split(','), 
     async execute(message) {
-        // debugging
         console.log(`Received message: ${message.content} from: ${message.author.username}`);
 
-        // only run in specific channel
         if (!this.channels.includes(message.channel.id)) { 
             return; 
         }
 
-        // add a new QuickLRU instance to the maps if the channel is new
         if (!this.messageMap.has(message.channel.id)) {
-            this.messageMap.set(message.channel.id, new QuickLRU({ maxSize: 1000 }));
+            this.messageMap.set(message.channel.id, {});
         }
         if (!this.urlMap.has(message.channel.id)) {
-            this.urlMap.set(message.channel.id, new QuickLRU({ maxSize: 1000 }));
+            this.urlMap.set(message.channel.id, {});
         }
 
-        // ignore bot messages
         if (message.author.bot || message.webhookId) return;
 
-        // check URLs
-        const urlsInMessage = message.content.match(/\bhttps?:\/\/\S+/gi); // Finds URLs
+        const urlsInMessage = message.content.match(/\bhttps?:\/\/\S+/gi);
 
-        if(urlsInMessage !== null){
+        if (urlsInMessage !== null) {
             for(let u of urlsInMessage){
-                if(this.urlMap.get(message.channel.id).has(u)){
-                    await message.channel.send('No duplicate URLs!').then(msg => {
-                            setTimeout(() => msg.delete(), 2000)  // Deletes the warning after 5 seconds
-                        });
+                if(this.urlMap.get(message.channel.id)[u]){
                     await message.delete();
-                    return ;
+                    return await message.channel.send('No duplicate URLs!').then(msg => {
+                        setTimeout(() => msg.delete(), 2000);
+                    });
                 }
-                this.urlMap.get(message.channel.id).set(u, true);
+                else {
+                    this.urlMap.get(message.channel.id)[u] = true;
+                }
             }
         }
 
-        // if this message is the same as the last one delete it
-        if (this.messageMap.get(message.channel.id).has(message.content)) {
+        if(this.messageMap.get(message.channel.id)[message.content]){
             await message.delete();
             console.log(`Deleted duplicated message: ${message.content}`);
-        } else {
-            this.messageMap.get(message.channel.id).set(message.content, true);
+        }
+        else {
+            this.messageMap.get(message.channel.id)[message.content] = true;
         }
     },
 };
