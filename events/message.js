65f01e8ae861eb28ccbc2b9c1f92e30cad880add
DigitalKittenz <114578234+DigitalKittenz@users.noreply.github.com>
2023-07-30 03:04:58 +0100

    added support for more than one channel . includes message.channel.id

--- events/message.js
+++ events/message.js
@@ -3,36 +3,51 @@ const QuickLRU = require('quick-lru');
 
 module.exports = {
     name: 'messageCreate',
-    messages: new QuickLRU({ maxSize: 1000 }),
-    urls: new QuickLRU({ maxSize: 1000 }),
+    messageMap: new Map(),
+    urlMap: new Map(),
+    channels: process.env.CHANNEL_IDS.split(','), 
     async execute(message) {
         // debugging
         console.log(`Received message: ${message.content} from: ${message.author.username}`);
+
+        // only run in specific channel
+        if (!this.channels.includes(message.channel.id)) { 
+            return; 
+        }
+
+        // add a new QuickLRU instance to the maps if the channel is new
+        if (!this.messageMap.has(message.channel.id)) {
+            this.messageMap.set(message.channel.id, new QuickLRU({ maxSize: 1000 }));
+        }
+        if (!this.urlMap.has(message.channel.id)) {
+            this.urlMap.set(message.channel.id, new QuickLRU({ maxSize: 1000 }));
+        }
 
         // ignore bot messages
         if (message.author.bot || message.webhookId) return;
 
         // check URLs
         const urlsInMessage = message.content.match(/\bhttps?:\/\/\S+/gi); // Finds URLs
-        
+
         if(urlsInMessage !== null){
             for(let u of urlsInMessage){
-                if(this.urls.has(u)){
+                if(this.urlMap.get(message.channel.id).has(u)){
+                    await message.channel.send('No duplicate URLs!').then(msg => {
+                            setTimeout(() => msg.delete(), 2000)  // Deletes the warning after 5 seconds
+                        });
                     await message.delete();
-                    return message.channel.send('No duplicate URLs!').then(msg => {
-                        setTimeout(() => msg.delete(), 5000)  // Deletes the warning after 5 seconds
-                    });
+                    return ;
                 }
-                this.urls.set(u, true);
+                this.urlMap.get(message.channel.id).set(u, true);
             }
         }
 
         // if this message is the same as the last one delete it
-        if (this.messages.has(message.content)) {
+        if (this.messageMap.get(message.channel.id).has(message.content)) {
             await message.delete();
             console.log(`Deleted duplicated message: ${message.content}`);
         } else {
-            this.messages.set(message.content, true);
+            this.messageMap.get(message.channel.id).set(message.content, true);
         }
-    }
+    },
 };
