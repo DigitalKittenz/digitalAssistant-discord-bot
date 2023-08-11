a19db0866cf6b86c1b4c371d7b439fc5a5806f9b
DigitalKittenz <114578234+DigitalKittenz@users.noreply.github.com>
2023-08-11 02:25:32 +0100

    added interaction.channel.id to the goodbye.js

--- commands/goodbye.js
+++ commands/goodbye.js
@@ -28,8 +28,8 @@ module.exports = {
         try {
             await interaction.reply(options[Math.floor(Math.random() * options.length)]);
 
-            // Set the autoReply state to false
-            client.globalState.autoReply = false;
+            // Set the autoReply state to false for this channel
+            client.globalState.autoReply[interaction.channel.id] = false;
         } catch (error) {
             console.error('Failed to reply to interaction:', error);
         }
