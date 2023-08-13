eb86457012d426c7c8205db1f4912fa734ef23fc
DigitalKittenz <114578234+DigitalKittenz@users.noreply.github.com>
2023-08-13 16:00:24 +0100

    added more random stuff to server info

--- commands/server.js
+++ commands/server.js
@@ -1,13 +1,27 @@
 module.exports = {
     name: 'server',
-    description: 'displays server info',
-    async execute(interaction, client) {
-        try {
-            await interaction.reply(`Server name: ${interaction.guild.name}\nTotal members: ${interaction.guild.memberCount}`);
-        }
-        catch (error) {
-            console.error(error);
-            await interaction.reply('There was an error while executing this command!');
-        }
+    description: 'displays some server info',
+    execute(message) {
+        const server = message.guild;
+        const serverCreatedDate = new Date(server.createdTimestamp).toDateString();
+        const owner = server.owner.user.username;
+        const roleCount = server.roles.cache.size;
+        const textChannelCount = server.channels.cache.filter(c => c.type === 'GUILD_TEXT').size;
+        const voiceChannelCount = server.channels.cache.filter(c => c.type === 'GUILD_VOICE').size;
+        const region = server.region;
+        const boostLevel = server.premiumTier;
+        const boostCount = server.premiumSubscriptionCount;
+
+        message.channel.send(`Server name: ${server.name}\n` +
+            `Server owner: ${owner}\n` +
+            `Created on: ${serverCreatedDate}\n` +
+            `Total members: ${server.memberCount}\n` +
+            `Roles: ${roleCount}\n` +
+            `Text Channels: ${textChannelCount}\n` +
+            `Voice Channels: ${voiceChannelCount}\n` +
+            `Region: ${region}\n` +
+            `Boost Level: ${boostLevel}\n` +
+            `Boost Count: ${boostCount}`
+        );
     },
 };
