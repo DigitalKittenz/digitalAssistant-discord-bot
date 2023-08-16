85df794f93204261dfa9a9f4bddcd847edb2d946
DigitalKittenz <114578234+DigitalKittenz@users.noreply.github.com>
2023-08-16 15:51:41 +0100

    made prompt better and deleted useless code

--- index.js
+++ index.js
@@ -127,19 +127,6 @@ for (let i = 0; i < client.globalState.conversations[message.channel.id].length;
 }
 
 
-/*let totalTokens = 0;
-for (let i = 0; i < client.globalState.conversations[message.channel.id].length; i++){
-    const messageContent = client.globalState.conversations[message.channel.id][i].content;
-    // count tokens (attempt to)
-    const tokensInMessage = countTokens(messageContent); // u need to define this function tho!
-    totalTokens += tokensInMessage;
-    // if the total token count exceeds the limit, cut the convo a bit
-    if (totalTokens >= 2000) {
-        client.globalState.conversations[message.channel.id] = client.globalState.conversations[message.channel.id].slice(i);
-        break;
-    }
-} */
-
     } catch (error) {
         console.error("oops got some errors: ", error);
     }
