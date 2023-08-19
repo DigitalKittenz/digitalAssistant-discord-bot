371ffa1da8f2b09a2bfbe4031be01601d386b223
DigitalKittenz <114578234+DigitalKittenz@users.noreply.github.com>
2023-08-19 15:06:31 +0100

    made good comments

--- events/message.js
+++ events/message.js
@@ -1,15 +1,20 @@
 module.exports = {
-    name: 'messageCreate',
-    messageMap: new Map(),
-    urlMap: new Map(),
-    channels: process.env.CHANNEL_IDS.split(','), 
+    name: 'messageCreate', // name of our event
+    messageMap: new Map(), // we're gonna use this map to keep track of messages
+    urlMap: new Map(), // and this one to keep track of urls
+    channels: process.env.CHANNEL_IDS.split(','), // here's a list of channels we care about
+    
+    // this is the main function that gets called when a message is created
     async execute(message) {
+        // let's print out the message and who sent it
         console.log(`Received message: ${message.content} from: ${message.author.username}`);
 
+        // if the message wasn't in one of our channels, we don't care about it
         if (!this.channels.includes(message.channel.id)) { 
             return; 
         }
 
+        // if we haven't seen this channel before, let's add it to our maps
         if (!this.messageMap.has(message.channel.id)) {
             this.messageMap.set(message.channel.id, {});
         }
@@ -17,28 +22,38 @@ module.exports = {
             this.urlMap.set(message.channel.id, {});
         }
 
+        // we don't care about messages from bots or webhooks
         if (message.author.bot || message.webhookId) return;
 
+        // let's find all the urls in the message
         const urlsInMessage = message.content.match(/\bhttps?:\/\/\S+/gi);
 
+        // if we found any urls...
         if (urlsInMessage !== null) {
+            // let's go through each one
             for(let u of urlsInMessage){
+                // if we've seen this url before in this channel...
                 if(this.urlMap.get(message.channel.id)[u]){
+                    // delete the message and tell the user no duplicate urls
                     await message.delete();
                     return await message.channel.send('No duplicate URLs!').then(msg => {
                         setTimeout(() => msg.delete(), 2000);
                     });
                 }
+                // if it's a new url, add it to our map
                 else {
                     this.urlMap.get(message.channel.id)[u] = true;
                 }
             }
         }
 
+        // if we've seen this message before in this channel...
         if(this.messageMap.get(message.channel.id)[message.content]){
+            // delete the message and print a log
             await message.delete();
             console.log(`Deleted duplicated message: ${message.content}`);
         }
+        // if it's a new message, add it to our map
         else {
             this.messageMap.get(message.channel.id)[message.content] = true;
         }
--- register.js
+++ register.js
@@ -1,44 +1,57 @@
+// this lil guy lets us chat with discord's REST API
 const { REST } = require('@discordjs/rest');
+
+//whip up the URLs for the API endpoints
 const { Routes } = require('discord-api-types/v9');
+
+// define our commands! each command is a cute lil object with a name and a description
 const commands = [
-    {
-        name: 'hello',
-        description: 'Says hello!'
-    },
-    {
-        name: 'goodbye',
-        description: 'says goodbye!'
-    },
-    {
-        name: 'manul',
-        description: 'Shows a manul pic',
-    },
-    {
-        name: 'botfact',
-        description: 'Tells a random fact!'
-    },
-    {
-        name: 'server',
-        description: 'displays server info',
-    },
-    {
-        name: 'help',
-        description: 'gives u help'
-    }
+{
+name: 'hello',
+description: 'Says hello!'
+},
+{
+name: 'goodbye',
+description: 'says goodbye!'
+},
+{
+name: 'manul',
+description: 'Shows a manul pic',
+},
+{
+name: 'botfact',
+description: 'Tells a random fact!'
+},
+{
+name: 'server',
+description: 'displays server info',
+},
+{
+name: 'help',
+description: 'gives u help'
+}
 
 ];
 
+// new REST object! we're tellin it to use version '9' and we're givin it our bot's secret token
 const rest = new REST({ version: '9' }).setToken(process.env.TOKEN);
 
+// here's a fancy thing called an immediately invoked function expression (IIFE)
+// this function that doesn't wait around, it gets to work as soon as it's defined
 (async () => {
     try {
+        // let's tell the world we're gettin started with reloadin slash commands
         console.log('Started reloading slash commands.');
+
+        // knocking on discord's door and handin over our commands
         await rest.put(Routes.applicationCommands(process.env.APP_ID), {
             body: commands,
         });
 
+        // if everything went well, we'll print a lil success message
         console.log('Successfully reloaded slash commands');
     } catch (error) {
+        // oh no! if something went wrong, we'll print the error so we can figure out what it is
         console.error(error);
     }
 })();
